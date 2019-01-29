1f0b5cb6899678396d1de9ba695edc4b4a91375f
fancaixia <1562045201@qq.com>
2019-01-29 15:43:17 +0800

    Update config.js

--- wx_static/config.js
+++ wx_static/config.js
@@ -2,8 +2,7 @@
  * 配置文件
  */
 const config = {
-  serverHost: "http://10.4.163.74:8080",  
-  // serverHost: "http://192.168.31.190:8080",  
+  serverHost: "http://你的本机IP:8080",  
 
 };
 export default config;
