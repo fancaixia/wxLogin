c9813e4a231f0ed0db75c1edc0a27538fa1330f4
fancaixia <1562045201@qq.com>
2019-01-29 15:33:31 +0800

    Update service.js

--- wx_static/service/service.js
+++ wx_static/service/service.js
@@ -8,24 +8,15 @@ const service = options => {
     dataType: "json",
     ...options,
     method: options.method ? options.method.toUpperCase() : "GET",
+    header:{
+      token: wx.getStorageSync("token") || '',
+    }
   };
   const result = new Promise(function(resolve, reject) {
     //做一些异步操作
     const optionsData = {
       success: res => {
         wx.hideNavigationBarLoading();
-        if (res.data.status == 1005){
-          wx.showModal({
-            title: '请登陆',
-            content: '您还未登录，请授权登陆',
-            success: res => {
-              app.reLogin();
-              wx.redirectTo({
-                url: '/pages/index',
-              });
-            }
-          });
-        }
         resolve(res.data);
       },
       fail: error => {
@@ -34,25 +25,6 @@ const service = options => {
       },
       ...options
     };
-
-    let token = wx.getStorageSync("token") || "";
-    if (!token) {
-      if (optionsData.url.indexOf('/api/login') == -1) {
-        wx.showModal({
-          title: '请登陆',
-          content: '您还未登录，请授权登陆',
-          success: res => {
-            app.reLogin();
-            wx.redirectTo({
-              url: '/index',
-            });
-          }
-        });
-        reject(error);
-        return;
-      }
-    }
-
     wx.request(optionsData);
   });
   return result;
